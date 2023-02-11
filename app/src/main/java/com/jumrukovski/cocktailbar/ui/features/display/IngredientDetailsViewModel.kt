df000205ef395ed0321bf94101027916deaf9f52
mendejumrukovski <mende@code.livelike.com>
2023-02-11 01:49:09 +0100

    refactoring viewModels

--- app/src/main/java/com/jumrukovski/cocktailbar/ui/features/display/IngredientDetailsViewModel.kt
+++ app/src/main/java/com/jumrukovski/cocktailbar/ui/features/display/IngredientDetailsViewModel.kt
@@ -2,11 +2,11 @@ package com.jumrukovski.cocktailbar.ui.features.display
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.jumrukovski.cocktailbar.domain.mapper.mapAsIngredientUIState
 import com.jumrukovski.cocktailbar.domain.usecase.SearchIngredientUseCase
-import com.jumrukovski.cocktailbar.domain.mapper.asIngredientUIState
 
 class IngredientDetailsViewModel(private val searchIngredient: SearchIngredientUseCase) :
     ViewModel() {
 
-    fun fetchData(name: String) = searchIngredient.invoke(name).asIngredientUIState(viewModelScope)
+    fun fetchData(name: String) = searchIngredient.invoke(name).mapAsIngredientUIState(viewModelScope)
 }
--- app/src/main/java/com/jumrukovski/cocktailbar/ui/features/filter/FilteredDrinksViewModel.kt
+++ app/src/main/java/com/jumrukovski/cocktailbar/ui/features/filter/FilteredDrinksViewModel.kt
@@ -2,12 +2,14 @@ package com.jumrukovski.cocktailbar.ui.features.filter
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.jumrukovski.cocktailbar.domain.usecase.GetFilteredDrinksUseCase
-import com.jumrukovski.cocktailbar.domain.mapper.asDrinksUIState
 import com.jumrukovski.cocktailbar.data.model.Filter
+import com.jumrukovski.cocktailbar.domain.mapper.mapAsDrinksUIState
+import com.jumrukovski.cocktailbar.domain.usecase.GetFilteredDrinksUseCase
 
 class FilteredDrinksViewModel(private val getFilteredDrinks: GetFilteredDrinksUseCase) :
     ViewModel() {
 
-    fun fetchItems(filter: Filter) = getFilteredDrinks.invoke(filter.param, filter.prepareTypeParam()).asDrinksUIState(viewModelScope)
+    fun fetchItems(filter: Filter) =
+        getFilteredDrinks.invoke(filter.param, filter.prepareTypeParam())
+            .mapAsDrinksUIState(viewModelScope)
 }
