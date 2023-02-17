5834d8062b0b258ff0b4333e525183abd7e13eb8
mendejumrukovski <mende@code.livelike.com>
2023-02-18 00:57:58 +0100

    improve IngredientDetailsActivity.kt data communication with the IngredientDetailsViewModel.kt to collect the data with StateFlow

--- app/src/main/java/com/jumrukovski/cocktailbar/ui/features/display/IngredientDetailsViewModel.kt
+++ app/src/main/java/com/jumrukovski/cocktailbar/ui/features/display/IngredientDetailsViewModel.kt
@@ -1,15 +1,25 @@
 package com.jumrukovski.cocktailbar.ui.features.display
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jumrukovski.cocktailbar.data.model.Ingredient
 import com.jumrukovski.cocktailbar.domain.usecase.SearchIngredientUseCase
 import com.jumrukovski.cocktailbar.ui.mapper.asFlowWithResult
 import com.jumrukovski.cocktailbar.ui.mapper.mapResponseResultToIngredientUIState
-import kotlinx.coroutines.flow.flow
+import com.jumrukovski.cocktailbar.ui.state.UIState
+import kotlinx.coroutines.flow.*
+import kotlinx.coroutines.launch
 
 class IngredientDetailsViewModel(private val searchIngredient: SearchIngredientUseCase) :
     ViewModel() {
 
-    suspend fun fetchData(name: String) = flow {
-        emit(searchIngredient.invoke(name).mapResponseResultToIngredientUIState())
-    }.asFlowWithResult()
+    private val _uiState: MutableStateFlow<UIState<Ingredient>> = MutableStateFlow(UIState.Loading(false))
+    val uiState: StateFlow<UIState<Ingredient>> = _uiState.asStateFlow()
+
+    fun requestData(name: String) {
+        viewModelScope.launch {
+            val responseResult = searchIngredient.invoke(name)
+            _uiState.emitAll(flowOf(responseResult.mapResponseResultToIngredientUIState()).asFlowWithResult())
+        }
+    }
 }
