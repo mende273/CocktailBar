6799c463890693bfbecaba6ed4b3d976550d337d
mendejumrukovski <mende@code.livelike.com>
2023-02-18 01:50:41 +0100

    improve FilterFragment.kt data communication with the FilterViewModel.kt to collect the data with StateFlow

--- app/src/main/java/com/jumrukovski/cocktailbar/ui/features/filter/FilterViewModel.kt
+++ app/src/main/java/com/jumrukovski/cocktailbar/ui/features/filter/FilterViewModel.kt
@@ -1,13 +1,22 @@
 package com.jumrukovski.cocktailbar.ui.features.filter
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.jumrukovski.cocktailbar.data.model.Drink
 import com.jumrukovski.cocktailbar.data.model.Filter
 import com.jumrukovski.cocktailbar.domain.usecase.GetFilterListUseCase
-import com.jumrukovski.cocktailbar.ui.mapper.asFlowWithResult
-import com.jumrukovski.cocktailbar.ui.mapper.mapResponseResultToDrinksUIState
-import kotlinx.coroutines.flow.flow
+import com.jumrukovski.cocktailbar.ui.mapper.mapResponseResultToDrinksUIStateFlow
+import com.jumrukovski.cocktailbar.ui.state.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.emitAll
+import kotlinx.coroutines.launch
 
 class FilterViewModel(private val getFilterList: GetFilterListUseCase) : ViewModel() {
+
+    private val _uiState:MutableStateFlow<UIState<List<Drink>>> = MutableStateFlow(UIState.Loading(true))
+    val uiState:StateFlow<UIState<List<Drink>>> = _uiState.asStateFlow()
 
     enum class FilterOption(val title: String, val param: String) {
         ALCOHOLIC("Alcoholic", "a"),
@@ -20,7 +29,7 @@ class FilterViewModel(private val getFilterList: GetFilterListUseCase) : ViewMod
 
     fun getSupportedFilters() = FilterOption.values().map { it.title }
 
-    fun setCurrentFilter(name: String) {
+    private fun setCurrentFilter(name: String) {
         FilterOption.values().forEach {
             if (it.title == name) {
                 currentFilter = it
@@ -28,9 +37,13 @@ class FilterViewModel(private val getFilterList: GetFilterListUseCase) : ViewMod
         }
     }
 
-    suspend fun fetchData() = flow {
-        emit(getFilterList.invoke(currentFilter.param, "list").mapResponseResultToDrinksUIState())
-    }.asFlowWithResult()
+    fun requestData(filter:String) {
+        setCurrentFilter(filter)
+        viewModelScope.launch {
+            val responseResult = getFilterList.invoke(currentFilter.param, "list")
+            _uiState.emitAll(responseResult.mapResponseResultToDrinksUIStateFlow())
+        }
+    }
 
     fun createFilter(value: String): Filter {
         return Filter(currentFilter.title, currentFilter.param, value)
