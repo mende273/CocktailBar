e369e7de7d2c01ceb620dc9592c43a7ff11935f5
mendejumrukovski <mende@code.livelike.com>
2023-02-26 02:07:24 +0100

    refactor deprecated decorView.systemUiVisibility

--- app/src/main/java/com/jumrukovski/cocktailbar/ui/base/BaseActivity.kt
+++ app/src/main/java/com/jumrukovski/cocktailbar/ui/base/BaseActivity.kt
@@ -3,11 +3,11 @@ package com.jumrukovski.cocktailbar.ui.base
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.view.WindowManager
 import android.widget.ProgressBar
 import android.widget.Toast
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 import androidx.lifecycle.ViewModel
 import androidx.viewbinding.ViewBinding
 import com.jumrukovski.cocktailbar.ui.views.CenteredToolbar
@@ -43,12 +43,8 @@ abstract class BaseActivity<VB : ViewBinding, VM : ViewModel> : AppCompatActivit
     }
 
     protected fun makeStatusBarTransparent() {
-        window.apply {
-            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
-            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-            statusBarColor = Color.TRANSPARENT
-        }
+        window.statusBarColor = Color.TRANSPARENT
+        WindowCompat.setDecorFitsSystemWindows(window, false)
     }
 
     protected fun showErrorMessage(message: String?) {
