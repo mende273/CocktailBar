f1ae32fda8384f9947d54be8033fef2ebabbf5fa
mendejumrukovski <mende@code.livelike.com>
2023-07-04 16:56:15 +0200

    add koin verify test

---
+++ app/src/test/java/com/jumrukovski/cocktailbar/CheckModules.kt
@@ -0,0 +1,22 @@
+package com.jumrukovski.cocktailbar
+
+import com.jumrukovski.cocktailbar.di.appModule
+import org.junit.Test
+import org.koin.core.annotation.KoinExperimentalAPI
+import org.koin.test.verify.verify
+
+class CheckModules {
+
+    @OptIn(KoinExperimentalAPI::class)
+    @Test
+    fun checkKoinModule() {
+
+        // Verify Koin configuration
+        appModule.verify(
+            extraTypes = listOf(
+                okhttp3.logging.HttpLoggingInterceptor.Logger::class,
+                java.io.File::class
+            )
+        )
+    }
+}
